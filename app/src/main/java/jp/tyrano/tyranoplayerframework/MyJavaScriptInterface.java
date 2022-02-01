0752cee5d3d1f0d95e46d6691c670026293a0f2a
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2022-02-01 13:44:02 +0800

    Remove unused JavaScript interfaces

--- app/src/main/java/jp/tyrano/tyranoplayerframework/MyJavaScriptInterface.java
+++ app/src/main/java/jp/tyrano/tyranoplayerframework/MyJavaScriptInterface.java
@@ -1,38 +1,19 @@
 package jp.tyrano.tyranoplayerframework;
 
-import android.util.Log;
 import android.webkit.JavascriptInterface;
-
-import org.json.JSONObject;
 
 /**
  * Created by nagatani on 2016/06/27.
  */
 public class MyJavaScriptInterface {
-    private static final String LOG_TAG = "TeachingFeeling";
-
     private MainActivity activity;
 
     public MyJavaScriptInterface(MainActivity activity) {
         this.activity = activity;
     }
 
-    // 音楽関係の命令取得
-    @JavascriptInterface
-    public void audio(String json_str) {
-        Log.e(LOG_TAG, "Audio: " + json_str);
-
-        try {
-            JSONObject obj = new JSONObject(json_str);
-            // context.audio(obj);
-        } catch (Exception e) {
-            Log.e(LOG_TAG, "JSON ERROR!!!", e);
-        }
-    }
-
     @JavascriptInterface
     public void setStorage(String key, String val) {
-        // セーブデータを保存する。
         activity.setStorage(key, val);
     }
 
@@ -49,9 +30,5 @@ public class MyJavaScriptInterface {
     @JavascriptInterface
     public void openUrl(String url) {
         activity.openUrl(url);
-    }
-
-    @JavascriptInterface
-    public void stopMovie() {
     }
 }
